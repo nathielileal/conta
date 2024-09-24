71ab3670fc27256ac2d1d02895e4725c6227ec5e
Nathieli Leal <nathieli@txfuel.com.br>
2024-09-24 10:19:11 -0300

    [Menu]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SomaComponent } from './pages/soma/soma.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'menu', component: MenuComponent },
+    { path: 'soma', component: SomaComponent },
+    { path: '', redirectTo: '/menu', pathMatch: 'full' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ src/app/pages/soma/soma.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soma',
+  standalone: true,
+  imports: [],
+  templateUrl: './soma.component.html',
+  styleUrl: './soma.component.css'
+})
+export class SomaComponent {
+
+}
